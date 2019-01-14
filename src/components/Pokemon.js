7e383ec355c4feb9157f214607f57622cb35fd53
Paloma Ania <paloma.aniabenito@gmail.com>
2019-01-14 15:33:45 +0100

    Print data without styles

---
+++ src/components/Pokemon.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Pokemon extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {types: this.props.types}
+    }
+    render() {
+        const typesList = this.state.types.map(member => <li>{member}</li>)
+      return (
+        <div className="container-pokemon">
+            <img src={this.props.url} alt=""></img>
+            <h1 className="h1">{this.props.name}</h1>
+            <ul className="types-list">{typesList}</ul>
+        </div>
+      );
+    }
+  }
+
+export default Pokemon;
