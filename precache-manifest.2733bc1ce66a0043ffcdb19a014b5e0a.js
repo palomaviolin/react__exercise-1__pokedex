1c09c0bccfb7c2cec98f0770263935f84bf94d24
Paloma Ania <paloma.aniabenito@gmail.com>
2021-02-01 21:01:34 +0100

    Updates

---
+++ precache-manifest.2733bc1ce66a0043ffcdb19a014b5e0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c3228aad64a4e578944",
+    "url": "/react__exercise-1__pokedex/static/js/runtime~main.1c3228aa.js"
+  },
+  {
+    "revision": "c06cf57a14ecd969153c",
+    "url": "/react__exercise-1__pokedex/static/js/main.c06cf57a.chunk.js"
+  },
+  {
+    "revision": "7c3aec79f3ae62290828",
+    "url": "/react__exercise-1__pokedex/static/js/1.7c3aec79.chunk.js"
+  },
+  {
+    "revision": "c06cf57a14ecd969153c",
+    "url": "/react__exercise-1__pokedex/static/css/main.09da266c.chunk.css"
+  },
+  {
+    "revision": "9c4987b4c2d6e8c3aa6e5e7423b6e6af",
+    "url": "/react__exercise-1__pokedex/index.html"
+  }
+];
